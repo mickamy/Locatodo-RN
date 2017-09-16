df28d7a9762b569eaa712abe0a3f5c481b3f19d5
Tetsuro Mikami <t.mikami1992@gmail.com>
2017-09-17 02:42:54 +0900

    Create stores

---
+++ src/stores/index.js
@@ -0,0 +1,10 @@
+// @flow
+
+import { createStore, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
+
+import reducers from '../reducers';
+
+const middleware = applyMiddleware(createLogger());
+
+export default createStore(reducers, middleware);
