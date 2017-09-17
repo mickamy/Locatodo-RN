9ed8d9ff27dd1f6f5d1139813dbb9cb4c50a7182
Tetsuro Mikami <t.mikami1992@gmail.com>
2017-09-17 20:34:44 +0900

    Add hamburger icon to navigation bar

--- src/scenes/todo/index.js
+++ src/scenes/todo/index.js
@@ -1,12 +1,19 @@
 // @flow
 
+import React from 'react';
 import { StackNavigator } from 'react-navigation';
 
+import { Hamburger } from '../../common/components';
 import TodoListScreen from './list/TodoListScreen';
+
+const drawerLeft = navigation => <Hamburger onDrawerOpen={() => navigation.navigate('DrawerOpen')} />;
 
 const routes = {
   TodoList: {
     screen: TodoListScreen,
+    navigationOptions: ({ navigation }) => ({
+      headerLeft: drawerLeft(navigation),
+    }),
   },
 };
 
